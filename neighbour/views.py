9cd565ac601cde9594f6bc78c8d8f82678e319c3
fuad manye / <fahadbasi333@gmail.com>
2022-06-21 11:47:01 +0300

    add views.py in neighbour

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from .models import Business, Location, Post
+from .forms import BusinessRegistrationForm
+from accounts.models import Profile, UserAccount
 
-# Create your views here.
+def home(request):
+    if request.user.is_authenticated: 
+        return redirect('landing')
+    return render(request, 'neighbour/index.html')
+
+@login_required(login_url='login')
+def landing(request):
+    user = UserAccount.objects.get(id = request.user.id)
+    bizna = Business.objects.all()
+    prof = user.users.all()
+    for profile in prof:
+        crispus = Location.objects.get(id = profile.location.id)
+        biashara = crispus.business.all()
+        bizna = biashara
+    location = user.people.all()
+    locations = Location.objects.all()
+    context = {'location': location, 'bizna':bizna, 'locations': locations}
+    return render(request, 'neighbour/landing.html', context)
+
+@login_required(login_url='login')
+def business(request):
+    businesses = Business.objects.all()
+
+    if request.method == 'POST':
+        form = BusinessRegistrationForm(request.POST, request.FILES)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            category = form.cleaned_data['category']
+            image = form.cleaned_data['image']
+            location = form.cleaned_data['location']
+            user = request.user
+
+
+            business = Business.objects.create(
+                name = name,
+                category = category,
+                image = image,
+                location = location,
+                user = user
+            )
+            location = Location.objects.all().filter(name = location)
+            for location in location:
+                business = location.location.add(business)
+
+            # business.save()
+
+            return redirect('business')
+
+    form = BusinessRegistrationForm()
+    context = {'businesses': businesses, 'form':form}
+
+    return render(request, 'neighbour/business.html', context)
