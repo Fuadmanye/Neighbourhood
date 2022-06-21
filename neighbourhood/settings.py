1ff2d1792ec38819377469e3fc0dac6cb352c0eb
fuad manye / <fahadbasi333@gmail.com>
2022-06-21 12:29:31 +0300

    create static file

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -18,7 +18,7 @@ import cloudinary.uploader
 import cloudinary.api
 
 cloudinary.config( 
-  cloud_name = "djbgqj522", 
+cloud_name = "djbgqj522", 
   api_key = os.environ.get('api_key'), 
   api_secret = os.environ.get('api_secret') 
 )
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY='wesrdtfyvguhbj3456789'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
